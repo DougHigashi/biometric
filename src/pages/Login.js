54618bfc75e4720bcbec31877a8a9991320202ae
Douglas Higashi <higoshi.douglas@gmail.com>
2021-10-05 11:01:47 -0300

    Adicionado botões de geração, visualização e deleção da chave privada

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,19 +4,40 @@ import Icon from 'react-native-vector-icons/Ionicons';
 import ReactNativeBiometrics from 'react-native-biometrics';
 
 export default function Component() {
+
   const auth = async () => {
     ReactNativeBiometrics.simplePrompt({ promptMessage: 'Confirm fingerprint' })
       .then((resultObject) => {
         const { success } = resultObject
-
         if (success) {
           console.log('successful biometrics provided')
         } else {
           console.log('user cancelled biometric prompt')
         }
       })
-      .catch(() => {
-        console.log('biometrics failed')
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  const getKeys = () => {
+    ReactNativeBiometrics.createKeys('Confirm fingerprint')
+      .then((resultObject) => {
+        const { publicKey } = resultObject
+        console.log(publicKey)
+      })
+  }
+
+  const deleteKeys = () => {
+    ReactNativeBiometrics.deleteKeys()
+      .then((resultObject) => {
+        const { keysDeleted } = resultObject
+
+        if (keysDeleted) {
+          console.log('Successful deletion')
+        } else {
+          console.log('Unsuccessful deletion because there were no keys to delete')
+        }
       })
   }
 
@@ -24,11 +45,21 @@ export default function Component() {
   return (
     <View style={styles.container}>
 
-      <Text style={styles.text}>Authenticate please</Text>
+      <Text style={styles.textTop}>Authenticate</Text>
 
 
       <TouchableOpacity style={styles.logo} onPress={auth}>
         <Icon name="finger-print" color="white" size={128} />
+      </TouchableOpacity>
+
+      <Text style={styles.text}>Print keys</Text>
+      <TouchableOpacity style={styles.logo} onPress={getKeys}>
+        <Icon name="key-sharp" color="white" size={128} />
+      </TouchableOpacity>
+
+      <Text style={styles.text}>Delete keys</Text>
+      <TouchableOpacity style={styles.logo} onPress={deleteKeys}>
+        <Icon name="close-circle-sharp" color="white" size={128} />
       </TouchableOpacity>
     </View>
 
@@ -46,12 +77,20 @@ const styles = StyleSheet.create({
   text: {
     textAlign: "center",
     color: "#fff",
-    marginVertical: 60,
+    marginTop: 15,
+    fontSize: 30,
+    fontFamily: "sans-serif-thin"
+  },
+  textTop: {
+    textAlign: "center",
+    color: "#fff",
+    marginTop: 90,
     fontSize: 30,
     fontFamily: "sans-serif-thin"
   },
   logo: {
-    alignItems: "center"
+    alignItems: "center",
+    marginTop: 10
   }, button: {
     alignItems: "center",
     backgroundColor: "#DDDDDD",
